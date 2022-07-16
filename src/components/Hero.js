ba650788956b244b5072bb6c39647555b451cd2f
Todd Evans <evansmtodd@gmail.com>
2022-07-16 12:52:55 +1000

    Installed framer motion

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
-import { Link } from 'gatsby'
-import React, { useState, useEffect } from 'react'
-import HeroVideo from '../images/HeroVideo.webm'
+import { Link } from 'gatsby';
+import React, { useState, useEffect } from 'react';
+import HeroVideo from '../images/HeroVideo.webm';
+import { motion } from 'framer-motion';
 
 export default function Hero() {
-  const [word, setWord] = useState('intuitive');
+  const [word, setWord] = useState('impactful');
 
+  // Updates the highlighted word every 3 seconds
   useEffect(() => {
-    const words = ['intuitive', 'fast', 'attractive', 'modern', 'blazing', 'fun', 'quality', 'beautiful', 'impactful', 'effective', 'scalable']
+    const words = ['intuitive', 'fast', 'attractive', 'modern', 'performant', 'fun', 'quality', 'beautiful', 'impactful', 'effective', 'scalable', 'functional']
     let count = 0;
 
     const interval = setInterval(() => {
@@ -22,7 +24,7 @@ export default function Hero() {
 
 
   return (
-    <div className='py-16 grid-cols-1 md:grid-cols-2'>
+    <div className='my-16 grid-cols-1 md:grid-cols-2'>
       <div>
         <video className='rounded-md' playsInline autoPlay muted loop>
           <source src={HeroVideo} type="video/webm" />
@@ -30,10 +32,9 @@ export default function Hero() {
       </div>
       <div className='text-center -mt-6'>
         <h1 className='text-5xl'>Todd Evans</h1>
+        {/* Consider making the below one <p> with a <span> for the highlight */}
         <p className='mt-8 mb-2 text-lg'>Developer who loves building</p>
-        <div className='m-auto mb-2 bg-lime w-20 text-dark_gray text-lg'>
-          <i>{ word }</i>
-        </div>
+        <div className='m-auto mb-2 bg-lime w-24 text-dark_gray text-lg'><motion.i initial={{y:'50%', opacity: 0, scale: 0.5}}>{ word }</motion.i></div>
         <p className='text-lg mb-10'>websites.</p>
         <Link to='/' className='btn'>See more</Link>
       </div>
