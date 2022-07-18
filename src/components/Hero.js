072edd7577f4c724e6153ae146bde7c7ee0fb0a0
Todd Evans <evansmtodd@gmail.com>
2022-07-18 13:29:37 +1000

    Added scroll down button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -36,18 +36,22 @@ export default function Hero() {
         {/* Consider making the below one <p> with a <span> for the highlight */}
         <p className='mt-8 mb-2 text-lg'>Developer who loves building</p>
         <div className='m-auto max-w-fit max-h-10 mb-2 bg-lime p-2 pb-9 rounded-md -skew-x-12 text-dark_gray text-lg'>
-          <AnimatePresence>
-            <motion.p
-              key={ word }
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{duration: 1, ease: 'easeInOut'}}
-              >{ word }
-            </motion.p>
-          </AnimatePresence>
+          <motion.p
+            key={ word }
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{duration: 1, ease: 'easeInOut'}}
+            >{ word }
+          </motion.p>
         </div>
         <p className='text-lg mb-10'>websites.</p>
-        <Link to='/' className='btn'>See more</Link>
+        <Link to='/'>
+          <svg class="arrows">
+							<path class="a1" d="M0 0 L30 32 L60 0"></path>
+							<path class="a2" d="M0 20 L30 52 L60 20"></path>
+							<path class="a3" d="M0 40 L30 72 L60 40"></path>
+					</svg>
+        </Link>
       </div>
     </div>
   )
