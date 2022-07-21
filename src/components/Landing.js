fc853d8ba615c28daa7c02a920a83cee7b674f70
Todd Evans <evansmtodd@gmail.com>
2022-07-21 10:40:13 +1000

    Put landing page highlight in a <span>

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -26,20 +26,21 @@ export default function Landing() {
     <section id='home' className='min-h-screen relative p-4 m-auto'>
       <MemoizedHero />
       <div className='m-auto my-60 absolute z-1'>
-        <div className='text-center'>
+        <div className=''>
           <h1 className='text-5xl'>Todd Evans</h1>
-          {/* Consider making the below one <p> with a <span> for the highlight */}
-          <p className='mt-8 mb-2 text-lg'>Developer who loves building</p>
-          <div className='m-auto max-w-fit max-h-10 mb-2 bg-lime p-2 pb-9 rounded-md -skew-x-12 text-dark_gray text-lg'>
-            <motion.p
-              key={ word }
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{duration: 1, ease: 'easeInOut'}}
-              >{ word }
-            </motion.p>
-          </div>
-          <p className='text-lg mb-10'>websites.</p>
+          <p className='mt-8 mb-2 text-lg'>Developer who loves building
+            <span>
+              <div className='max-w-fit max-h-10 mb-2 bg-lime p-2 pb-9 rounded-md -skew-x-12 text-dark_gray text-lg'>
+                <motion.p
+                  key={ word }
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{duration: 1, ease: 'easeInOut'}}
+                  >{ word }
+                </motion.p>
+              </div>
+            </span>
+          websites.</p>
         </div>
           <AnchorLink to='/#about'>
             <svg class="arrows">
