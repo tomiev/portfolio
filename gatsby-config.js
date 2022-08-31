82e62be352d5fa008c53a1bf3e52dc5ebc01fc20
Todd Evans <evansmtodd@gmail.com>
2022-08-31 13:28:19 +1000

    Strapi configuration for Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig ={
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ['project'],
+  singleTypes: [],
+};
+
 module.exports = {
   siteMetadata: {
     title: `Todd Evans | Software Developer`,
