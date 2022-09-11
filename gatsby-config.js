99f95ebd46939cbf5fa72c1fa93da299119844ab
Todd Evans <evansmtodd@gmail.com>
2022-09-11 17:00:12 +1000

    Fixed strapi/gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,8 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const strapiConfig ={
-  apiURL: process.env.STRAPI_API_URL,
-  accessToken: process.env.STRAPI_TOKEN,
-  collectionTypes: [`project`],
-  singleTypes: [],
-};
+const path = require(`path`);
 
 module.exports = {
   siteMetadata: {
@@ -23,15 +18,18 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        "name": "images",
-        "path": "./src/images/"
+        name: "images",
+        path: path.join(__dirname, `src`, `images`),
       },
-
-    __key: "images"
     },
     {
       resolve: `gatsby-source-strapi`,
-      options: strapiConfig,
+      options: {
+        apiURL: process.env.GATSBY_STRAPI_API_URL,
+        accessToken: process.env.GATSBY_STRAPI_TOKEN,
+        queryLimit: 1000,
+        collectionTypes: [`project`],
+      }
     },
   ],
 };
